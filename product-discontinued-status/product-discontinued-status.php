16159cc46daa680e9774a46058ad81279317331a
Stephen Huang <stephenhuang8@gmail.com>
2024-08-19 10:50:07 -0700

    New Added Feature - users can still buy remaining stock

--- product-discontinued-status/product-discontinued-status.php
+++ product-discontinued-status/product-discontinued-status.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Refined WooCommerce Product and Variation Discontinued Status
 Description: Adds improved discontinued status for WooCommerce products and their variations.
-Version: 1.1
+Version: 1.2
 Author: Stephen Huang
 */
 
@@ -45,14 +45,14 @@ function save_variation_discontinued_field($variation_id) {
 }
 add_action('woocommerce_save_product_variation', 'save_variation_discontinued_field', 10, 1);
 
-// Display Discontinued status on product page
+// Modify the display_discontinued_status function
 function display_discontinued_status() {
     global $product;
 
     $product_discontinued = get_post_meta($product->get_id(), '_discontinued', true);
 
     if ($product_discontinued === 'yes') {
-        echo '<div class="discontinued-banner">Product Discontinued</div>';
+        echo '<div class="discontinued-banner">Product Discontinued - Limited Stock Available</div>';
     } elseif ($product->is_type('variable')) {
         $variations = $product->get_available_variations();
         echo '<div class="discontinued-variations">';
@@ -65,7 +65,7 @@ function display_discontinued_status() {
                     $taxonomy = str_replace('attribute_', '', $key);
                     $term = get_term_by('slug', $value, $taxonomy);
                     if ($term) {
-                        echo '<span class="discontinued">' . $term->name . ': Discontinued</span>';
+                        echo '<span class="discontinued">' . $term->name . ': Discontinued - Limited Stock Available</span>';
                     }
                 }
             }
@@ -75,15 +75,40 @@ function display_discontinued_status() {
 }
 add_action('woocommerce_single_product_summary', 'display_discontinued_status', 5);
 
-// Display discontinued status on shop page
+// Modify the display_shop_discontinued_status function
 function display_shop_discontinued_status() {
     global $product;
     $product_discontinued = get_post_meta($product->get_id(), '_discontinued', true);
     if ($product_discontinued === 'yes') {
-        echo '<span class="discontinued">Discontinued</span>';
+        echo '<span class="discontinued">Discontinued - Limited Stock</span>';
     }
 }
 add_action('woocommerce_before_shop_loop_item_title', 'display_shop_discontinued_status');
+
+// Replace the hide_discontinued_variations function with this new one
+function manage_discontinued_variations($is_available, $variation) {
+    $product_discontinued = get_post_meta($variation->get_parent_id(), '_discontinued', true);
+    $variation_discontinued = get_post_meta($variation->get_id(), '_variation_discontinued', true);
+    
+    if (($product_discontinued === 'yes' || $variation_discontinued === 'yes') && $variation->get_stock_quantity() <= 0) {
+        return false;
+    }
+    return $is_available;
+}
+add_filter('woocommerce_variation_is_active', 'manage_discontinued_variations', 10, 2);
+
+// Add a new function to modify the "Add to Cart" button text for discontinued products
+function modify_add_to_cart_text($text, $product) {
+    $product_discontinued = get_post_meta($product->get_id(), '_discontinued', true);
+    
+    if ($product_discontinued === 'yes' && $product->is_in_stock()) {
+        return __('Buy Remaining Stock', 'woocommerce');
+    }
+    
+    return $text;
+}
+add_filter('woocommerce_product_single_add_to_cart_text', 'modify_add_to_cart_text', 10, 2);
+add_filter('woocommerce_product_add_to_cart_text', 'modify_add_to_cart_text', 10, 2);
 
 // Add styles for Discontinued label and banner
 function add_discontinued_styles() {
@@ -114,15 +139,3 @@ function add_discontinued_styles() {
     </style>';
 }
 add_action('wp_head', 'add_discontinued_styles');
-
-// Optionally hide discontinued variations from dropdown
-function hide_discontinued_variations($is_available, $variation) {
-    $product_discontinued = get_post_meta($variation->get_parent_id(), '_discontinued', true);
-    $variation_discontinued = get_post_meta($variation->get_id(), '_variation_discontinued', true);
-    
-    if ($product_discontinued === 'yes' || $variation_discontinued === 'yes') {
-        return false;
-    }
-    return $is_available;
-}
-add_filter('woocommerce_variation_is_active', 'hide_discontinued_variations', 10, 2);
